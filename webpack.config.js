63c3838f51faa11bdc79babf82f5f1bf8bfba66d
MyeongHoonNam <codenmh0822@naver.com>
2021-10-10 00:53:24 +0900

    chore: 웹팩 버그 해결

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+// const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   resolve: {
@@ -36,10 +36,10 @@ module.exports = {
     new HtmlPlugin({
       template: './src/index.html'
     }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'static' }
-      ]
-    })
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'static' }
+    //   ]
+    // })
   ],
 }
