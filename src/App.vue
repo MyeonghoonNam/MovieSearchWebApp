a3d4a6a95ec0087681c3fb62bcbb702b8d7ae8dd
MyeongHoonNam <codenmh0822@naver.com>
2021-10-10 02:37:59 +0900

    feat: App 기초 템플릿 구성

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app__inner">
+    <div class="app__page">
+      <Header />
+      <main></main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/layout/Header'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  * {
+    box-sizing: border-box;
+  }
+
+  .app__inner {
+    height: 100vh;
+    font-family: "Noto Sans KR", sans-serif;
+
+    .app__page {
+      // border: solid 1px red;
+      height: 100%;
+      display:flex;
+      flex-direction: column;
+      align-items: center;
+
+      // main {
+      //   border: solid 1px black;
+      // }
+    }
+  }
+
+</style>
