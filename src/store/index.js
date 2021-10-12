7963f0c7eeafdc7becb574f4f026cc95388f77cd
MyeongHoonNam <codenmh0822@naver.com>
2021-10-12 22:37:54 +0900

    init: 사용자 입력에 따른 상태 변경 적용

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,29 @@ const API_KEY = process.env.API_KEY
 const API_END_POINT = process.env.API_END_POINT
 
 export default createStore({
+  state() {
+    return {
+      movieList: {},
+    }
+  },
+  mutations: {
+    updateState(state, newState) {
+      state = {
+        ...state,
+        ...newState
+      }
+    }
+  },
   actions: {
-    async fetchMovieList( context, data = {}) {
+    async fetchMovieList({ commit }, data = {}) {
       const { userInput } = data
       const movieList = await fetch(`${API_END_POINT}?apikey=${API_KEY}&s=${userInput}`).then(res => res.json())
 
       console.log(movieList)
+
+      commit('updateState', {
+        movieList: movieList.Search,
+      })
     }
   }
 })
