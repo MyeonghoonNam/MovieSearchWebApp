247595574419f73bb643594cc497d48c2fb6c75f
MyeongHoonNam <codenmh0822@naver.com>
2021-10-12 13:41:06 +0900

    feat: 초기 index 설정

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+export default createRouter({
+  history: createWebHistory,
+  scrollBehavior: () => ({ top: 0 }),
+  routes: [
+    {
+      
+    }
+  ]
+})
