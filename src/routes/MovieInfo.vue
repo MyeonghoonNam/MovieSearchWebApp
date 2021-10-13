7fb77828b5e011c14a0edb4b8757f0a357697f13
MyeongHoonNam <codenmh0822@naver.com>
2021-10-13 18:20:11 +0900

    style: 영화 상세 정보 UI 구현

--- src/routes/MovieInfo.vue
+++ src/routes/MovieInfo.vue
@@ -1,23 +1,23 @@
 <template>
   <section class="movie">
     <div class="movie__info">
-      <div class="">
+      <div class="info title">
         {{ movieInfo.Title }}
       </div>
-      <div class="">
-        {{ movieInfo.Released }}
+      <div class="info">
+        <span>개봉일</span> {{ movieInfo.Released }}
       </div>
-      <div class="">
-        {{ movieInfo.Genre }}
+      <div class="info">
+        <span>장르</span> {{ movieInfo.Genre }}
       </div>
-      <div class="">
-        {{ movieInfo.Director }}
+      <div class="info">
+        <span>감독</span> {{ movieInfo.Director }}
       </div>
-      <div class="">
-        {{ movieInfo.Actors }}
+      <div class="info">
+        <span>배우</span> {{ movieInfo.Actors }}
       </div>
-      <div class="">
-        {{ movieInfo.Plot }}
+      <div class="info">
+        <span>줄거리</span><br /><br />{{ movieInfo.Plot }}
       </div>
     </div>
     <div class="movie__poster">
@@ -40,5 +40,39 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  .movie {
+    padding: 0 50px;
+    display: flex;
+    justify-content: center;
+
+    .movie__info {
+      width: 400px;
+      font-size: 14px;
+
+      .info {
+        margin: 10px;
+        padding: 5px 0;
+
+        span {
+          font-size: 18px;
+          font-weight: bold;
+        }
+      }
+      
+      .title {
+        font-size: 40px;
+        font-weight: bold;
+      }
+    }
+
+    .movie__poster {
+      width: 400px;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
 
 </style>
