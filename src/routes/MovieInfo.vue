fb54bf41b34d8e26eb4033afcfcf93a177c61605
MyeongHoonNam <codenmh0822@naver.com>
2021-10-13 17:43:04 +0900

    feat: 영화 상세 정보 컴포넌트 구현

---
+++ src/routes/MovieInfo.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="movie">
+    <div class="movie__info">
+      <div class="">
+        {{ movieInfo.Title }}
+      </div>
+      <div class="">
+        {{ movieInfo.Released }}
+      </div>
+      <div class="">
+        {{ movieInfo.Genre }}
+      </div>
+      <div class="">
+        {{ movieInfo.Director }}
+      </div>
+      <div class="">
+        {{ movieInfo.Actors }}
+      </div>
+      <div class="">
+        {{ movieInfo.Plot }}
+      </div>
+    </div>
+    <div class="movie__poster">
+      <img
+        :src="movieInfo.Poster"
+        alt="영화포스터"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    movieInfo() {
+      return this.$store.state.movieInfo
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
